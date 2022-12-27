50c1af7db7287f9ba8a3b8a012c3982203d5b0e5
ahmed <65723807+adel-habib@users.noreply.github.com>
2022-12-27 22:08:23 +0100

    tcp server with select

--- client.c
+++ client.c
@@ -1,18 +1,20 @@
-#include "headers.h"
-
-#if defined(_WIN32)
-#include <conio.h>
-#endif
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
-#if defined(_WIN32)
-    WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d)) {
-        fprintf(stderr, "Failed to initialize.\n");
-        return 1;
-    }
-#endif
 
     if (argc < 3) {
         fprintf(stderr, "usage: tcp_client hostname port\n");
@@ -42,8 +44,7 @@ int main(int argc, char *argv[]) {
 
     printf("Creating socket...\n");
     SOCKET socket_peer;
-    socket_peer = socket(peer_address->ai_family,
-            peer_address->ai_socktype, peer_address->ai_protocol);
+    socket_peer = socket(peer_address->ai_family,peer_address->ai_socktype, peer_address->ai_protocol);
     if (!ISVALIDSOCKET(socket_peer)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
@@ -51,8 +52,7 @@ int main(int argc, char *argv[]) {
 
 
     printf("Connecting...\n");
-    if (connect(socket_peer,
-                peer_address->ai_addr, peer_address->ai_addrlen)) {
+    if (connect(socket_peer,peer_address->ai_addr, peer_address->ai_addrlen)) {
         fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
@@ -90,26 +90,17 @@ int main(int argc, char *argv[]) {
                     bytes_received, bytes_received, read);
         }
 
-#if defined(_WIN32)
-        if(_kbhit()) {
-#else
+
         if(FD_ISSET(0, &reads)) {
-#endif
             char read[4096];
             if (!fgets(read, 4096, stdin)) break;
             printf("Sending: %s", read);
             int bytes_sent = send(socket_peer, read, strlen(read), 0);
             printf("Sent %d bytes.\n", bytes_sent);
         }
-    } //end while(1)
+    } 
 
     printf("Closing socket...\n");
     CLOSESOCKET(socket_peer);
-
-#if defined(_WIN32)
-    WSACleanup();
-#endif
-
-    printf("Finished.\n");
     return 0;
 }
