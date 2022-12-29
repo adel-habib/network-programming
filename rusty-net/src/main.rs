a9738b46647d78b40e95d9b871076a29473ffab0
ahmed <65723807+adel-habib@users.noreply.github.com>
2022-12-29 22:33:23 +0100

    broadcast messages via channel

--- rusty-net/src/main.rs
+++ rusty-net/src/main.rs
@@ -1,25 +1,39 @@
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener, sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listner = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let (tx, _rx) = broadcast::channel::<String>(10);
+    
     loop {
         let (mut socket, _addr) = listner.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
         tokio::spawn(async move {
             let (red, mut wrt) = socket.split();
             let mut reader = BufReader::new(red);
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        wrt.write_all(&msg.as_bytes()).await.unwrap();
+                    }
                 }
-                wrt.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
+
             }
         });
     }
